a937b552c3083f3a6ef89275bd2a2fb17d3d62c7
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-03-20 14:24:51 +0100

    feat: console.log milestone1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 // Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
 const teamMembersObjArray = [
     { name: 'Wayne Barnett', role: 'Founder & CEO', photo: 'assets/img/wayne-barnett-founder-ceo.jpg' },
     { name: 'Agela Caroll', role: 'Chief Editor', photo: 'assets/img/angela-caroll-chief-editor.jpg' },
@@ -8,10 +10,17 @@ const teamMembersObjArray = [
     { name: 'Frank Gigli', role: 'Developer', photo: 'assets/img/frank-gigli-developer.jpg' },
     { name: 'Barbara Ramos', role: 'Graphic Designer', photo: 'assets/img/barbara-ramos-graphic-designer.jpg' },
 ]
-// MILESTONE 0:
-// Creare l’array di oggetti con le informazioni fornite.
 // MILESTONE 1:
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+for(let i = 0; i < teamMembersObjArray.length; i++) {
+    const member = teamMembersObjArray[i];
+    console.log(`Nome: ${member.name}`);
+    console.log(`Ruolo: ${member.role}`);
+    console.log(`Foto: ${member.photo}\n`);
+}
+
+
+
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
 // BONUS 1:
