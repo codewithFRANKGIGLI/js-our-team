9e3ff66891e54326779e388d111a39a47011e898
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-03-20 14:16:52 +0100

    feat: developer member & array

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+const teamMembersObjArray = [
+    { name: 'Wayne Barnett', role: 'Founder & CEO', photo: 'assets/img/wayne-barnett-founder-ceo.jpg' },
+    { name: 'Agela Caroll', role: 'Chief Editor', photo: 'assets/img/angela-caroll-chief-editor.jpg' },
+    { name: 'Walter Gordon', role: 'Office Manager', photo: 'assets/img/walter-gordon-office-manager.jpg' },
+    { name: 'Angela Lopez', role: 'Social Media Manager', photo: 'assets/img/angela-lopez-social-media-manager.jpg' },
+    { name: 'Frank Gigli', role: 'Developer', photo: 'assets/img/frank-gigli-developer.jpg' },
+    { name: 'Barbara Ramos', role: 'Graphic Designer', photo: 'assets/img/barbara-ramos-graphic-designer.jpg' },
+]
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
